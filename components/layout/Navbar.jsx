ebe5e32d55c037eb78bf46a51dfa39d8169dd0ed
ehsan <ehsantahamii@gmail.com>
2025-03-25 13:56:23 +0330

    commit #26

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -26,10 +26,10 @@ const Navbar = () => {
     return (
         <nav className="w-full h-20 flex justify-center items-center bg-white shadow-sm">
             <div className='w-[85%] flex items-center justify-center mx-auto'>
-                <div className='w-[70%] font-bold'>
+                <div className='w-[50%] lg:w-[70%] font-bold'>
                     LOGO
                 </div>
-                <div className='w-[30%] flex justify-end'>
+                <div className='w-[50%] lg:w-[30%] flex justify-end'>
 
                     {
                         renderNavBtn()
